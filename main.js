d32c106c605d1ff04b02b2d8484d602c122d72c9
ManuPedreros <manuelpedreros@msn.com>
2023-01-12 17:29:58 -0500

    Fusión mobile-menu al index, Ajustes Media query para vistas desktop-menu y mobile-menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const navIconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navBarEmail.addEventListener('click', toggleDesktopMenu);
+navIconMenu.addEventListener('click', toggleMobilepMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobilepMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
