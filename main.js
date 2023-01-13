3cfe76d95abe90a0ee34317ee08df210c3e00b7d
ManuPedreros <manuelpedreros@msn.com>
2023-01-12 22:49:15 -0500

    Fusion de carrito de compras al index, funcionalidades para ocultar o mostrar mobile-menu, desktop-menu y asideCart

--- main.js
+++ main.js
@@ -2,14 +2,42 @@ const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const navIconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const navIconShopCart = document.querySelector('.navbar-shopping-cart');
+const asideCart = document.querySelector('.product-detail');
 
 navBarEmail.addEventListener('click', toggleDesktopMenu);
 navIconMenu.addEventListener('click', toggleMobilepMenu);
+navIconShopCart.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isCarritoAsideClosed = asideCart.classList.contains('inactive')
+
+    if (!isCarritoAsideClosed) {
+        asideCart.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobilepMenu() {
+    const isCarritoAsideClosed = asideCart.classList.contains('inactive')
+    
+    if (!isCarritoAsideClosed) {
+        asideCart.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    } else if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    asideCart.classList.toggle('inactive')
 }
